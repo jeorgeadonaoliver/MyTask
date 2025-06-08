724bf73f032b05b83d2ec687b4c73b4aa97916af
Jeorge Oliver <jeorgeadonaoliver@gmail.com>
2025-06-09 02:06:22 +0800

    Commit 2

--- mytask-nextjs/src/app/layout.tsx
+++ mytask-nextjs/src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { ToastContainer } from 'react-toastify';
 import { QueryProvider } from './query-provider';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
@@ -5,6 +6,17 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <body>
         <QueryProvider>{children}</QueryProvider>
+         <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
       </body>
     </html>
   );
